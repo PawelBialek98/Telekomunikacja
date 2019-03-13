0796c29e40b36d939c49478622ef186a1900801b
Paweł Białek <pawelbialek@MacBook-Pro-Pawe.local>
2019-03-13 18:14:26 +0100

    jestem wylewem

--- Zadanie1/src/Pojedynczy.java
+++ Zadanie1/src/Pojedynczy.java
@@ -1,14 +1,38 @@
+import java.util.ArrayList;
+
 public class Pojedynczy {
 
     private int H[][]= {{1,1,1,1,1,1,1,1,1,0,0,0},
                         {1,1,1,1,1,1,1,1,0,1,0,0},
                         {1,1,1,1,1,1,1,1,0,0,1,0},
-                        {1,1,1,1,1,1,1,1,0,0,0,1}};
+                        {1,1,1,1,1,1,1,1,0,0,0,1}};  //To jeszcze do poprawki ofc
+
+    //private ArrayList<int> T = new ArrayList<int>();
+    private int T[] = new int[8];
 
     public String encrypt(String input){
 
-        System.out.println("ELO!");
+        //int pom = Integer.parseInt(input);
+        int pom = Integer.parseInt(input);
+
+        //System.out.println("ELO!");
+        stringToInt(input);
+        System.out.println(pom);
+        for(int i=0; i<8; i++){
+            System.out.print(T[i]);
+        }
 
         return input;
     }
+
+    public void stringToInt(String input){
+        int pom = Integer.parseInt(input);
+        for(int i=7; i>=0; i++){
+            System.out.println(pom%10);
+            if(pom%10 == 0) { T[i] = 0;}
+            else { T[i] = 1;}
+            pom /= 10;
+        }
+    }
+
 }
