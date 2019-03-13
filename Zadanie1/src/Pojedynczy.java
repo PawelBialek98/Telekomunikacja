6247cf25211d7b498cb760ba15c6bbf23bae908a
Paweł Białek <pawelbialek@MacBook-Pro-Pawe.local>
2019-03-13 17:49:26 +0100

    first commit

---
+++ Zadanie1/src/Pojedynczy.java
@@ -0,0 +1,14 @@
+public class Pojedynczy {
+
+    private int H[][]= {{1,1,1,1,1,1,1,1,1,0,0,0},
+                        {1,1,1,1,1,1,1,1,0,1,0,0},
+                        {1,1,1,1,1,1,1,1,0,0,1,0},
+                        {1,1,1,1,1,1,1,1,0,0,0,1}};
+
+    public String encrypt(String input){
+
+        System.out.println("ELO!");
+
+        return input;
+    }
+}
