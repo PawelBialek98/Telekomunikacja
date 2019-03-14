ea39e0aa6a6f159bb81ed5e1d067ad70566beaca
Paweł Białek <pawelbialek@MacBook-Pro-Pawe.local>
2019-03-14 20:18:57 +0100

    Punkt 1

--- Zadanie1/src/Main.java
+++ Zadanie1/src/Main.java
@@ -8,93 +8,14 @@ import javafx.stage.Stage;
 import javafx.scene.control.*;
 
 
-public class Main extends Application {
-
-    Stage window;
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        window=primaryStage;
-        window.setTitle("Hello World");     //cale te trzy linijki co jest window to tworzymy jakby nasze okno
-
-        GridPane grid = new GridPane();     //tworzymy nową siatkę żeby ładnie mozna było porozkładać wszystkie elementy
-        grid.setPadding(new Insets(10, 10, 10, 10));        //pokazujemy na ile elementow dzielimy siatke
-        grid.setVgap(5);
-        grid.setHgap(5); //ustalamy poziome i pionowe odleglosci
-
-        Label singleLabel = new Label("Podaj ciąg 8 bitów do stworzenia słów kodowych");
-        GridPane.setConstraints(singleLabel, 0, 0);       //tworzymy nowa etykiete i mowimy gdzie ma sie znajdowac
-
-        TextField singleInput = new TextField();
-        singleInput.setPrefWidth(300);
-        GridPane.setConstraints(singleInput, 0, 1);       //tworzymy nowe pole tekstowe i mowimy gdzie ma sie znajdowac
-
-        Button buttonSingle = new Button("Wykonaj");
-        GridPane.setConstraints(buttonSingle, 1,1);        //tworzymy nowy guzik i mowimy gdzie ma sie znajdowac
-
-        Label inputLabel = new Label("Tu wyświetlimy stworzone słowo kodowe, zmien 1 bit i sprawdź");
-        GridPane.setConstraints(inputLabel, 0, 2);      //tworzymy nowa etykiete i mowimy gdzie ma sie znajdowac
-
-        TextField userInput = new TextField();
-        GridPane.setConstraints(userInput, 0, 3);        //tworzymy nowe pole tekstowe i mowimy gdzie ma sie znajdowac
-
-        Button buttonInput = new Button("Wykonaj");
-        GridPane.setConstraints(buttonInput, 1,3);       //tworzymy nowy guzik i mowimy gdzie ma sie znajdowac
-
-        Label cipherLabel = new Label("Zaszyfrowany tekst");
-        GridPane.setConstraints(cipherLabel, 0, 4);     //tworzymy nowa etykiete i mowimy gdzie ma sie znajdowac
-
-        TextField cipherOutput = new TextField();
-        GridPane.setConstraints(cipherOutput, 0, 5);    //tworzymy nowe pole tekstowe i mowimy gdzie ma sie znajdowac
-
-        Label decipherLabel = new Label("Ponownie odszyfrowany tekst");
-        GridPane.setConstraints(decipherLabel, 0, 6);       //tworzymy nowa etykiete i mowimy gdzie ma sie znajdowac
-
-        TextField decipherOutput = new TextField();
-        GridPane.setConstraints(decipherOutput, 0, 7);      //tworzymy nowe pole tekstowe i mowimy gdzie ma sie znajdowac
-
-        //OneTimePad cipherInput = new OneTimePad();      //tworzymy nowy obiekt klasy onetimepad czyli naszego algorytmu
-        Pojedynczy pojedynczy = new Pojedynczy();
-
-        buttonSingle.setOnAction(e -> {                                              //tutaj beda rzeczy co sie beda dzialy po wcisnieciu guzika ktory zwie sie button imput
-            //cipherInput.setInput(userInput.getText());                              //ustawaimy w naszym obiekcie typu onetimepad tekst jawny jako zawartosc pola tekstowego co sie zwie userinput
-            //System.out.println(cipherInput.getInput());
-            //cipherInput.getKey();                                                   //losujemy se klucza
-            //System.out.println(cipherInput.key);
-            //String zaszyfrowany = cipherInput.cipher(cipherInput.getInput());
-            //System.out.println(zaszyfrowany);
-            // System.out.println(cipherInput.cipher(zaszyfrowany));
-
-            //cipherOutput.setText(cipherInput.cipher(cipherInput.getInput()));       //szyfrujemy i wyswietlamy
-            //cipherInput.saveToFile();
-            //System.out.println(cipherInput.cipher(cipherInput.getInput()));
-            //decipherOutput.setText(cipherInput.cipher(cipherOutput.getText()));     //odszyfrujemy i wyswietlamy
-            // System.out.println(cipherInput.cipher(cipherOutput.getText()));
-            //pojedynczy.encrypt(singleInput.getText());
-            userInput.setText(pojedynczy.encrypt(singleInput.getText()));
-        });
-
-        buttonInput.setOnAction(e -> {
-
-           /* cipherInput.setInput(cipherInput.setFileInput(fileInput.getText()));   // To samo zo wyżej, tylko czytam z okienka fileInput
-            cipherInput.getKey();
-            cipherOutput.setText(cipherInput.cipher(cipherInput.getInput()));
-            decipherOutput.setText(cipherInput.cipher(cipherOutput.getText()));*/
-            //BitSet bitSet = new BitSet();
-            //BitSet.valueOf(cipherInput.setFileInput(fileInput.getText()));
-
-        });
-
-
-        grid.getChildren().addAll(singleLabel,singleInput,buttonSingle, inputLabel, userInput, buttonInput, cipherLabel, cipherOutput, decipherLabel, decipherOutput); //dodajemy wszystko do siaty
-
-        Scene scene = new Scene(grid, 400, 375);        //ustawiamy "scene" i podajemy wymiary
-        window.setScene(scene);     //przekazujemy ja do naszego okna
-        window.show();      //i wyswietlamy
-    }
+public class Main {
 
 
     public static void main(String[] args) {
-        launch(args);
+        Pojedynczy pojedynczy = new Pojedynczy();
+        pojedynczy.encrypt();
+        pojedynczy.changeT();
+        pojedynczy.decrypt();
+
     }
 }
--- Zadanie1/src/Pojedynczy.java
+++ Zadanie1/src/Pojedynczy.java
@@ -1,37 +1,104 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
 
 public class Pojedynczy {
 
-    private int H[][]= {{1,1,1,1,1,1,1,1,1,0,0,0},
-                        {1,1,1,1,1,1,1,1,0,1,0,0},
-                        {1,1,1,1,1,1,1,1,0,0,1,0},
-                        {1,1,1,1,1,1,1,1,0,0,0,1}};  //To jeszcze do poprawki ofc
+    private int H[][]= {{1,1,1,0,1,0,0,0,1,0,0,0},
+                        {1,1,0,1,0,1,1,0,0,1,0,0},
+                        {1,0,1,1,0,1,0,1,0,0,1,0},
+                        {0,1,1,1,1,0,1,1,0,0,0,1}};  //To jeszcze do poprawki ofc
 
-    //private ArrayList<int> T = new ArrayList<int>();
-    private int T[] = new int[8];
+    //private ArrayList<Integer> T = new ArrayList<Integer>();
+    private ArrayList<Integer> T = new ArrayList<>(Arrays.asList(1, 0, 1, 1, 0, 1, 1, 1));
+    private ArrayList<Integer> E = new ArrayList<>();
+    //private int T[] = new int[8];
 
-    public String encrypt(String input){
+    private int calculate_C(int r){
+        int c = 0;
+        for(int i=0;i<T.size();i++){
+            c += H[r][i] * T.get(i);
+        }
+        c %= 2;
+        return c;
+    }
 
-        //int pom = Integer.parseInt(input);
-        int pom = Integer.parseInt(input);
 
-        //System.out.println("ELO!");
-        stringToInt(input);
-        System.out.println(pom);
-        for(int i=0; i<8; i++){
-            System.out.print(T[i]);
+    public void encrypt(){
+
+        for(int i=0;i<4;i++){
+            int C = calculate_C(i);
+            T.add(C);
         }
 
-        return input;
+        System.out.println("Zaszyfrowana wiadomosc!: " + T);
+        System.out.println("Wysyłam");
+        System.out.println();
     }
+
+    private void repair(){
+        boolean theSame = false;
+        //int pom = 0;
+
+        for(int i=0; i<T.size(); i++){
+            for(int j=0; j<E.size(); j++){
+                if(H[j][i] == E.get(j)){
+                    theSame = true;
+                }
+                else{
+                    theSame = false;
+                    break;
+                }
+            }
+            if(theSame){
+                System.out.println("Znalazłem bład na pozycji: " + i);
+                int bit = T.get(i);
+                if(bit == 1) bit = 0;
+                else bit = 1;
+                T.remove(i);
+                T.add(i,bit);
+            }
+        }
+    }
+
+    public void decrypt(){
+        System.out.println("Odebrana wiadomosc: " + T);
+        boolean blad = false;
+
+        for(int i=0;i<4;i++){
+            int c = calculate_C(i);
+            E.add(c);
+            if(c==1) blad = true;
+        }
+
+        if(blad){
+            System.out.println("W wiadomości wystąpił błąd!");
+            System.out.println("Naprawię go!");
+            repair();
+        }
+
+        System.out.println("Poprawna wiadomosc: " + T);
+    }
+
+    public void changeT(){
+        T.remove(3);
+        T.add(3,0);
+    }
+
+
+
+
+
+
 
     public void stringToInt(String input){
         int pom = Integer.parseInt(input);
-        for(int i=7; i>=0; i++){
-            System.out.println(pom%10);
-            if(pom%10 == 0) { T[i] = 0;}
-            else { T[i] = 1;}
-            pom /= 10;
+        int pom2 = 11001110;
+        for(int i=7; i>=0; i--){
+            System.out.println(pom2%10);
+            if(pom2%10 == 0) { T.add(0);}
+            else { T.add(1);}
+            pom2 /= 10;
         }
     }
 
