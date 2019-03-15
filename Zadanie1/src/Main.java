bf6a60650344908a763d108a9a3b443a2ca0bbe3
Paweł Białek <pawelbialek@MacBook-Pro-Pawe.local>
2019-03-15 18:55:04 +0100

    Ale gunwo sie zrobilo

--- Zadanie1/src/Main.java
+++ Zadanie1/src/Main.java
@@ -14,14 +14,18 @@ public class Main {
     public static void main(String[] args) {
         Pojedynczy pojedynczy = new Pojedynczy();
         Files file = new Files();
-        byte [] kod = file.readFromFile("C:\\Users\\Łukasz\\Desktop\\wiadomosc.txt");
+        //byte [] kod = file.readFromFile("C:\\Users\\Łukasz\\Desktop\\wiadomosc.txt");
+        byte [] kod = file.readFromFile("wiadomosc.txt");
         pojedynczy.BytesToVector(kod);
         pojedynczy.encrypt();
         //System.out.println(pojedynczy.getT().get(4));
-        pojedynczy.changeT();
-        byte [] out = pojedynczy.VectorToBytes();
-        file.saveToFile(out,"C:\\Users\\Łukasz\\Desktop\\wiadomosc2.txt");
+        //pojedynczy.changeT();
+
+        //file.saveToFile(out,"C:\\Users\\Łukasz\\Desktop\\wiadomosc2.txt");
+
         pojedynczy.decrypt();
+        byte [] out = pojedynczy.VectorToBytes();
+        file.saveToFile(out,"wiadomosc2.txt");
 
     }
 }
--- Zadanie1/src/Pojedynczy.java
+++ Zadanie1/src/Pojedynczy.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 
+
 public class Pojedynczy {
 
     private int H[][]= {{1,1,1,0,1,0,0,0,1,0,0,0},
@@ -12,23 +13,36 @@ public class Pojedynczy {
     //private ArrayList<Integer> T = new ArrayList<Integer>();
     private ArrayList<Integer> T = new ArrayList<>();
     private ArrayList<Integer> E = new ArrayList<>();
+    private int ArraySize;
     //private int T[] = new int[8];
 
-    private int calculate_C(int r){
+    private int calculate_C(int r,int k){
         int c = 0;
-        for(int i=0;i<T.size();i++){
-            c += H[r][i] * T.get(i);
+        for(int i=0;i<8;i++){
+            c += H[r][i] * T.get(k*8+i);
         }
         c %= 2;
         return c;
     }
 
+    private int calculate_C2(int r,int k){
+        int c = 0;
+        for(int i=0;i<12;i++){
+            c += H[r][i] * T.get(k*8+i);
+        }
+        c %= 2;
+        return c;
+    }
 
     public void encrypt(){
 
-        for(int i=0;i<4;i++){
-            int C = calculate_C(i);
-            T.add(C);
+        for(int k=0;k<ArraySize;k++){
+            for(int i=0;i<4;i++){
+                int C = calculate_C(i,k);
+                T.add((k+1)*8+k*4+i, C);
+
+            }
+            System.out.println(T);
         }
 
         System.out.println("Zaszyfrowana wiadomosc!: " + T);
@@ -36,11 +50,11 @@ public class Pojedynczy {
         System.out.println();
     }
 
-    private void repair(){
+    private void repair(int k){
         boolean theSame = false;
         //int pom = 0;
 
-        for(int i=0; i<T.size(); i++){
+        for(int i=0; i<12; i++){
             for(int j=0; j<E.size(); j++){
                 if(H[j][i] == E.get(j)){
                     theSame = true;
@@ -51,33 +65,42 @@ public class Pojedynczy {
                 }
             }
             if(theSame){
-                System.out.println("Znalazłem bład na pozycji: " + i);
-                int bit = T.get(i);
+                System.out.println("Znalazłem bład na pozycji: " + k*8+i);
+                int bit = T.get(k*8+i);
                 if(bit == 1) bit = 0;
                 else bit = 1;
-                T.remove(i);
-                T.add(i,bit);
+                T.remove(k*8+i);
+                T.add(k*8+i,bit);
             }
         }
     }
 
     public void decrypt(){
         System.out.println("Odebrana wiadomosc: " + T);
-        boolean blad = false;
+        boolean blad;
 
-        for(int i=0;i<4;i++){
-            int c = calculate_C(i);
-            E.add(c);
-            if(c==1) blad = true;
+        for(int k=0;k<ArraySize;k++) {
+            blad = false;
+            for (int i = 0; i < 4; i++) {
+                int c = calculate_C2(i, k);
+                E.add(c);
+                System.out.println("C na pozycji " + i + "  " + c);
+                if (c == 1) blad = true;
+            }
+
+            if (blad) {
+                System.out.println("W wiadomości wystąpił błąd!");
+                System.out.println("Naprawię go!");
+                repair(k);
+            }
+            System.out.println(E);
+            E.clear();
+
+            for (int i = 0; i < 4; i++) {
+                T.remove((k + 1) * 8);
+            }
         }
-
-        if(blad){
-            System.out.println("W wiadomości wystąpił błąd!");
-            System.out.println("Naprawię go!");
-            repair();
-        }
-
-        System.out.println("Poprawna wiadomosc: " + T);
+        System.out.println(T);
     }
 
     public void changeT(){
@@ -93,6 +116,11 @@ public class Pojedynczy {
             if(!bity.get(i)) T.add(0);
             else T.add(1);
         }
+        ArraySize = tab.length;
+        System.out.println(tab.length);
+        System.out.println(bity.size());
+        System.out.println(T);
+
     }
 
     public byte[] VectorToBytes(){
