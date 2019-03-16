1bc5f7225faf4a453733d8bef70616a58d45ffb2
lukko98@gmail.com <lukko98@gmail,com>
2019-03-16 20:12:00 +0100

    Stworzenie interfejsu graficznego

--- Zadanie1/src/Main.java
+++ Zadanie1/src/Main.java
@@ -8,26 +8,72 @@ import javafx.stage.Stage;
 import javafx.scene.control.*;
 
 
-public class Main {
+public class Main extends Application{
 
+    Stage window;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+        window.setTitle("Podpis cyfrowy");
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(15, 15, 15, 15));
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        Label sendLabel = new Label("Podaj ścieżkę do pliku do wyslania");
+        GridPane.setConstraints(sendLabel, 0, 0);
+
+        TextField fileInput = new TextField();
+        fileInput.setPrefWidth(300);
+        GridPane.setConstraints(fileInput, 0, 1);
+
+        Button buttonSend = new Button("Wyslij");
+        GridPane.setConstraints(buttonSend, 1,1);
+
+        Label verifyLabel = new Label("Podaj ścieżkę gdzie zapisac zmodyfikowany plik do wyslania");
+        GridPane.setConstraints(verifyLabel, 0,2);
+
+        TextField verifyInput = new TextField();
+        verifyInput.setPrefWidth(300);
+        GridPane.setConstraints(verifyInput, 0,3);
+
+        Label receiveLabel = new Label("Podaj ścieżkę do odbioru pliku");
+        GridPane.setConstraints(receiveLabel, 0,4);
+
+        TextField receiveInput = new TextField();
+        verifyInput.setPrefWidth(300);
+        GridPane.setConstraints(receiveInput, 0,5);
+
+        Button buttonReceive = new Button("Odbierz");
+        GridPane.setConstraints(buttonReceive, 1,5);
+
+        Files files = new Files();
+        Pojedynczy pojedynczy = new Pojedynczy();
+
+        buttonSend.setOnAction(event -> {
+            byte[] kod = files.readFromFile(fileInput.getText());
+            pojedynczy.BytesToVector(kod);
+            pojedynczy.encrypt();
+            pojedynczy.changeT();
+            byte[] wysylka = pojedynczy.VectorToBytes();
+            files.saveToFile(wysylka, verifyInput.getText());
+        });
+
+        buttonReceive.setOnAction(event ->{
+            pojedynczy.decrypt();
+            byte[] out = pojedynczy.VectorToBytes();
+            files.saveToFile(out,receiveInput.getText());
+        });
+
+        grid.getChildren().addAll(sendLabel,fileInput,buttonSend,verifyLabel, verifyInput, receiveInput, receiveLabel, buttonReceive);
+        Scene scene = new Scene(grid, 430, 250);
+        window.setScene(scene);
+        window.show();
+    }
 
     public static void main(String[] args) {
-        Pojedynczy pojedynczy = new Pojedynczy();
-        Files file = new Files();
-        //byte [] kod = file.readFromFile("C:\\Users\\Łukasz\\Desktop\\wiadomosc.txt");
-        byte [] kod = file.readFromFile("wiadomosc.txt");
-        pojedynczy.BytesToVector(kod);
-        pojedynczy.encrypt();
-
-        pojedynczy.changeT();
-        byte[] wysylka = pojedynczy.VectorToBytes();
-        //file.saveToFile(out,"C:\\Users\\Łukasz\\Desktop\\wiadomosc2.txt");
-        file.saveToFile(wysylka,"wiadomoscDoWysylki.txt");
-
-        pojedynczy.decrypt();
-        byte [] out = pojedynczy.VectorToBytes();
-        file.saveToFile(out,"wiadomoscOdszyfrowana.txt");
-
-
+        launch(args);
     }
 }
